2ec685db4fa7ff3829a3bee31d3c21946368b629
tanjian1998 <386974698@qq.com>
2017-06-27 23:23:08 +0800

    event listener 不能超过11个的警告问题，app。js中添加了 require('events').EventEmitter.prototype._maxListeners = 100;

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var server = require('http').createServer(app)
 server.listen(8012);
 
 
-
+require('events').EventEmitter.prototype._maxListeners = 100;
 
 //载入所有的http路由处理模块
 var routes = require('./routes/index');
